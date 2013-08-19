e6a11968d320cfb6dc5ee5296d43507bb13c0c60
Victor Koronen <koronen@kth.se>
2013-08-19 21:39:13 +0200

    Add i18n support

--- Gemfile
+++ Gemfile
@@ -1,10 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'rake', '~> 10.1.0'
-
 gem 'sinatra', '~> 1.4.3'
-
 gem 'haml', '~> 4.0.1'
+gem 'i18n', '~> 0.6.4'
 
 group :assets do
   gem 'sinatra-asset-pipeline', '~> 0.2.0'
--- penning.rb
+++ penning.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
 require 'sinatra/asset_pipeline'
 
+require 'i18n'
+I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config/locales', '*.yml').to_s]
+
 class Penning < Sinatra::Base
   register Sinatra::AssetPipeline
 
-  set :assets_prefix, 'assets'
+  helpers do
+    def t(*args)
+      I18n.t(*args)
+    end
+  end
+
+  before do
+    I18n.locale = ENV['LOCALE'].to_s.empty? ? :en : ENV['LOCALE'].to_sym
+  end
 
   get '/' do
     haml :index
